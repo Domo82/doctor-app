328fdd20d63a5537eb4dd8eea04330066857fef2
Domo82 <domodillon82@gmail.com>
2020-05-14 18:37:13 +0100

    style searchbar

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -17,7 +17,105 @@ import { Patient } from '../patients.model';
 })
 export class SearchPage implements OnInit {
 
-  
+  term = '';
+  filterData = [
+    {
+      address : "138 Ballyshannon",
+      allergies : "na",
+      creatorId : "Dr Dillon",
+      creatorName : "12345",
+      dateOfBirth : "1993-01-28T18:16:29.992Z",
+      drugHistory : "insulin ",
+      emergencyContact1 : 123,
+      emergencyContact2 : 456,
+      emergencyContact3 : 789,
+      forename : "Teresa",
+      imageUrl : "/assets/images/newuser.png",
+      medicalHistory : "Heart Condition,\nDiabetes\n",
+      rfid : 39384710,
+      surname : "Dillon"
+    },
+     {
+      address : "22 Main Street",
+      allergies : "na",
+      creatorId : "12345",
+      creatorName : "Dr Dillon",
+      dateOfBirth : "1982-02-01T10:39:10.351Z",
+      drugHistory : "na",
+      emergencyContact1 : 9,
+      emergencyContact2 : 98,
+      emergencyContact3 : 87,
+      forename : "Jane",
+      imageUrl : "/assets/images/newuser.png",
+      medicalHistory : "na",
+      rfid : 96968364,
+      surname : "Doe"
+    },
+     {
+      address : "Dublin",
+      allergies : "na",
+      creatorId : "Dr Dillon",
+      creatorName : "12345",
+      dateOfBirth : "1987-02-01T11:28:42.592Z",
+      drugHistory : "na",
+      emergencyContact1 : 123,
+      emergencyContact2 : 321,
+      emergencyContact3 : 456,
+      forename : "John",
+      imageUrl : "/assets/images/newuser.png",
+      medicalHistory : "na",
+      rfid : 74673742,
+      surname : "Doe"
+    },
+     {
+      address : "Coolock",
+      allergies : "na",
+      creatorId : "Dr Dillon",
+      creatorName : "12345",
+      dateOfBirth : "1984-02-08T13:45:04.329Z",
+      drugHistory : "na",
+      emergencyContact1 : 12,
+      emergencyContact2 : 12,
+      emergencyContact3 : 12,
+      forename : "Dominic",
+      imageUrl : "/assets/images/dominic.png",
+      medicalHistory : "na",
+      rfid : 38453942,
+      surname : "Dillon"
+    },
+    //  {
+    //   address : "Springfield",
+    //   allergies : "na",
+    //   creatorId : "Dr Dillon",
+    //   creatorName : "12345",
+    //   dateOfBirth : "1984-03-01T17:19:34.735Z",
+    //   drugHistory : "na",
+    //   emergencyContact1 : 1,
+    //   emergencyContact2 : 2,
+    //   emergencyContact3 : 3,
+    //   forename : "Stephen",
+    //   imageUrl : "/assets/images/newuser.png",
+    //   medicalHistory : "na",
+    //   rfid : 96968364,
+    //   surname : "Wilson"
+    // },
+     {
+      "address" : "street",
+      "allergies" : "na",
+      "creatorId" : "Dr Dillon",
+      "creatorName" : "12345",
+      "dateOfBirth" : "1968-03-01T19:37:31.945Z",
+      "drugHistory" : "na",
+      "emergencyContact1" : 1,
+      "emergencyContact2" : 2,
+      "emergencyContact3" : 3,
+      "forename" : "Michael",
+      "imageUrl" : "/assets/images/newuser.png",
+      "medicalHistory" : "na",
+      "rfid" : 97003148,
+      "surname" : "O'Brien"
+    }
+  ]
 
   loadedPatients: Patient[];
   private patientsSubscription: Subscription;
